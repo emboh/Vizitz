55d3afaffa37a16fa65f60e87338d771adf7dcc1
Hans Richard <emboh.mail@gmail.com>
2021-05-16 12:15:40 +0700

    Update Role.cs

--- Entities/Role.cs
+++ Entities/Role.cs
@@ -6,7 +6,7 @@ namespace Vizitz.Entities
 {
     public class Role : IdentityRole<Guid>
     {
-        public const string Admin = "Admin";
+        public const string Administrator = "Administrator";
 
         public const string Visitor = "Visitor";
 
