0696482946b45daed8804a72a3ee29c50ee7716f
Hans Richard <emboh.mail@gmail.com>
2021-07-23 05:14:40 +0700

    Update RoleConfiguration.cs
    remove hardcode

--- Data/RoleConfiguration.cs
+++ Data/RoleConfiguration.cs
@@ -21,22 +21,22 @@ namespace Vizitz.Data
                 new Role
                 {
                     Id = new Guid("b6f768d5-6d77-4814-8a93-a679f97b6448"),
-                    Name = "Administrator",
-                    NormalizedName = "ADMINISTRATOR",
+                    Name = Role.Administrator,
+                    NormalizedName = Role.Administrator.ToUpper(),
                     ConcurrencyStamp = "b6f768d5-6d77-4814-8a93-a679f97b6448",
                 },
                 new Role
                 {
                     Id = new Guid("1fe125cd-2a32-4a6e-aed9-7ff821627b38"),
-                    Name = "Proprietor",
-                    NormalizedName = "PROPRIETOR",
+                    Name = Role.Proprietor,
+                    NormalizedName = Role.Proprietor.ToUpper(),
                     ConcurrencyStamp = "1fe125cd-2a32-4a6e-aed9-7ff821627b38",
                 },
                 new Role
                 {
                     Id = new Guid("889ef87a-ba2c-4e6e-b71c-03786981e437"),
-                    Name = "Visitor",
-                    NormalizedName = "VISITOR",
+                    Name = Role.Visitor,
+                    NormalizedName = Role.Visitor.ToUpper(),
                     ConcurrencyStamp = "889ef87a-ba2c-4e6e-b71c-03786981e437",
                 }
             );
