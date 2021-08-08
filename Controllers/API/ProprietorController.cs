962d8e8018f2bcdb1b13592f955da5c3a67b0140
Hans Richard <emboh.mail@gmail.com>
2021-08-08 22:32:34 +0700

    Update ProprietorController.cs

--- Controllers/API/ProprietorController.cs
+++ Controllers/API/ProprietorController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,9 @@ using System.Threading.Tasks;
 using Vizitz.Entities;
 using Vizitz.IRepository;
 using Vizitz.Models;
+using Vizitz.Models.Paginate;
 using Vizitz.Services;
+using X.PagedList;
 
 namespace Vizitz.Controllers.API
 {
@@ -21,8 +24,6 @@ namespace Vizitz.Controllers.API
     public class ProprietorController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
-
-        private readonly RoleManager<Role> _roleManager;
 
         private readonly ILogger<ProprietorController> _logger;
 
@@ -34,7 +35,6 @@ namespace Vizitz.Controllers.API
 
         public ProprietorController(
             UserManager<User> userManager,
-            RoleManager<Role> roleManager,
             ILogger<ProprietorController> logger,
             IMapper mapper,
             IAuthManager authManager,
@@ -42,8 +42,6 @@ namespace Vizitz.Controllers.API
             )
         {
             _userManager = userManager;
-
-            _roleManager = roleManager;
 
             _logger = logger;
 
@@ -57,18 +55,23 @@ namespace Vizitz.Controllers.API
         [AllowAnonymous]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<IEnumerable<ProprietorDTO>>> GetProprietor()
+        public async Task<ActionResult<IEnumerable<ProprietorDTO>>> GetProprietor([FromQuery] RequestParams requestParams)
         {
-            var role = await _roleManager.FindByNameAsync(Role.Proprietor);
+            var proprietors = await _unitOfWork.Proprietors.GetPagedList(requestParams);
 
-            var proprietors = await _userManager.Users
-                .Include(u => u.UserRoles.Where(r => r.RoleId == role.Id))
-                .ThenInclude(ur => ur.Role)
-                .AsNoTracking()
-                .ToListAsync();
+            var metadata = new
+            {
+                proprietors.Count,
+                proprietors.PageSize,
+                proprietors.PageCount,
+                proprietors.PageNumber,
+                proprietors.HasNextPage,
+                proprietors.HasPreviousPage,
+            };
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
 
             return Ok(_mapper.Map<IList<ProprietorDTO>>(proprietors));
         }
@@ -81,28 +84,17 @@ namespace Vizitz.Controllers.API
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<ProprietorDTO>> GetProprietor(Guid id)
         {
-            User proprietor = await _userManager.Users
-                    .Include(u => u.UserRoles)
-                    .ThenInclude(ur => ur.Role)
-                    .FirstOrDefaultAsync(u => u.Id == id);
+            var proprietor = await _unitOfWork.Proprietors.Get(u => u.Id == id);
 
             if (proprietor == null)
             {
                 return NotFound();
             }
 
-            var roles = await _userManager.GetRolesAsync(proprietor);
-
-            // TODO : check roles inside include operation
-            if (!roles.Contains(Role.Proprietor))
-            {
-                return Problem($"User Role is not {Role.Proprietor}.", statusCode: StatusCodes.Status400BadRequest);
-            }
-
             return _mapper.Map<ProprietorDTO>(proprietor);
         }
 
-        [Authorize(Roles = Role.Administrator)]
+        [Authorize(Roles = $"{Role.Administrator}")]
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -132,8 +124,7 @@ namespace Vizitz.Controllers.API
             return CreatedAtRoute(nameof(GetProprietor), new { id = proprietor.Id }, _mapper.Map<ProprietorDTO>(proprietor));
         }
 
-        // TODO : Use constant from Role instead of hardcode
-        [Authorize(Roles = "Administrator,Proprietor")]
+        [Authorize(Roles = $"{Role.Administrator},{Role.Proprietor}")]
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -143,9 +134,7 @@ namespace Vizitz.Controllers.API
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> PutProprietor(Guid id, [FromBody] UpdateProprietorDTO proprietorDTO)
         {
-            User proprietor = await _userManager.FindByIdAsync(id.ToString());
-
-            Venue venue = await _unitOfWork.Venues.Get(q => q.Id == id);
+            var proprietor = await _unitOfWork.Proprietors.Get(u => u.Id == id);
 
             if (proprietor == null)
             {
@@ -156,12 +145,16 @@ namespace Vizitz.Controllers.API
 
             _mapper.Map(proprietorDTO, proprietor);
 
-            await _userManager.UpdateAsync(proprietor);
+            _unitOfWork.Proprietors.Update(proprietor);
+
+            await _unitOfWork.Save();
+
+            //await _userManager.UpdateAsync(proprietor);
 
             return NoContent();
         }
 
-        [Authorize(Roles = Role.Administrator)]
+        [Authorize(Roles = $"{Role.Administrator}")]
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -171,10 +164,7 @@ namespace Vizitz.Controllers.API
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteProprietor(Guid id)
         {
-            User proprietor = await _userManager.Users
-                    .Include(u => u.UserRoles)
-                    .ThenInclude(ur => ur.Role)
-                    .FirstOrDefaultAsync(u => u.Id == id);
+            var proprietor = await _unitOfWork.Proprietors.Get(u => u.Id == id);
 
             if (proprietor == null)
             {
@@ -183,7 +173,11 @@ namespace Vizitz.Controllers.API
                 return NotFound();
             }
 
-            await _userManager.DeleteAsync(proprietor);
+            await _unitOfWork.Proprietors.Delete(id);
+
+            await _unitOfWork.Save();
+
+            //await _userManager.DeleteAsync(proprietor);
 
             return NoContent();
         }
