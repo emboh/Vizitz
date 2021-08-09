855611adcfdf88eea4175b0d8aeb1b4bfd0c78f8
Hans Richard <emboh.mail@gmail.com>
2021-08-09 20:23:41 +0700

    add policy requirement

---
+++ Services/Requirements/VerifiedProprietorRequirement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vizitz.Entities;
+
+namespace Vizitz.Services.Requirements
+{
+    public class VerifiedProprietorRequirement : IAuthorizationRequirement
+    {
+        public bool IsVerified { get; private set; }
+
+        public VerifiedProprietorRequirement()
+        {
+
+        }
+    }
+}
