d301b876d9aaecab429e9a70aacd6917ed23c7bc
Hans Richard <emboh.mail@gmail.com>
2021-09-19 18:58:34 +0700

    Update UserRoleDTO.cs

--- Models/UserRoleDTO.cs
+++ Models/UserRoleDTO.cs
@@ -4,15 +4,6 @@ namespace Vizitz.Models
 {
     public class UserRoleDTO
     {
-        [IgnoreDataMember]
-        public string UserId { get; set; }
-
-        [IgnoreDataMember]
-        public UserDTO User { get; set; }
-
-        [IgnoreDataMember]
-        public string RoleId { get; set; }
-
         public RoleDTO Role { get; set; }
     }
 }
